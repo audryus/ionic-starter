6f8e35316c8228efa95bf947fe0ed08b4e747cf3
Aubay Aml <amleite@aubay.com>
2020-03-29 10:25:49 +0100

    Added loader

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
 import { MovieService } from '../services/movie/movie.service';
 
 @Component({
@@ -9,16 +11,36 @@ import { MovieService } from '../services/movie/movie.service';
 })
 export class FeedPage implements OnInit {
   lista_filmes:Array<any>;
+  loader:HTMLIonLoadingElement;
 
-  constructor(public movieService:MovieService) { }
+  constructor(public movieService:MovieService, 
+    public loadingController: LoadingController) { }
 
-  ngOnInit() {
-    this.movieService.getPopularMovies().subscribe(data => {
-      const response = (data as any)
-      this.lista_filmes = response.results;
-    }, error => {
-      console.log(error);
-    })
+  ngOnInit() { }
+
+  ionViewDidEnter() {
+    this.presentLoading().then(() => {
+      this.movieService.getPopularMovies().subscribe(data => {
+        const response = (data as any)
+        this.lista_filmes = response.results;
+        this.closeLoading();
+      }, error => {
+        console.log(error);
+        this.closeLoading();
+      });
+
+    });
+  }
+
+  async presentLoading():Promise<void> {
+    this.loader = await this.loadingController.create({
+      message: 'Please wait...'
+    });
+    return await this.loader.present();
+  }
+
+  async closeLoading():Promise<boolean> {
+    return await this.loader.dismiss();
   }
 
 }
